66d89bba7c7245b40d7f359ee5a2ec627f70ff67
Katerina Paltoglou <katpaltoglou@gmail.com>
2020-05-14 17:24:03 +0300

    transforming library usages

--- js/game/drawer.js
+++ js/game/drawer.js
@@ -1,16 +1,32 @@
-define(['domReady!'], function(doc) {
-    var cvs = doc.getElementById('gameBoard'),
+define(['domReady'], function(doc) {
+    
+    var returnedObj;
+
+    doc(function() {
+
+        // console.log(this)
+        var cvs = document.getElementById('gameBoard'),
         ctx = cvs.getContext('2d'),
         clearArea;
 
-    clearArea = function() {
-        ctx.clearRect(0, 0, cvs.width, cvs.height);
-    }
+        clearArea = function() {
+            ctx.clearRect(0, 0, cvs.width, cvs.height);
+        }
 
-    return {
-        ctx: ctx,
-        canvasWidth: cvs.width,
-        canvasHeight: cvs.height,
-        clearArea: clearArea,
-    };
+        // return {
+        //     ctx: ctx,
+        //     canvasWidth: cvs.width,
+        //     canvasHeight: cvs.height,
+        //     clearArea: clearArea,
+        // };
+
+        returnedObj = {
+            ctx: ctx,
+            canvasWidth: cvs.width,
+            canvasHeight: cvs.height,
+            clearArea: clearArea,
+        };
+    })
+
+    return returnedObj;
 })
--- js/game/game.js
+++ js/game/game.js
@@ -1,65 +1,70 @@
-require(['domReady!', 'drawer', 'board', 'paddle', 'ball',
+require(['domReady', 'drawer', 'board', 'paddle', 'ball',
     'constants', 'conf'],
     function(doc, drawer, board, paddle, ball, constants, conf) {
-        var startGame, animate, gameLoop, endGame, paddleMove,
+        
+        doc(function() {
+
+            var startGame, animate, gameLoop, endGame, paddleMove,
             score = 0, gameStatus = constants.GAME.STATUS.STOP;
 
-        startGame = function() {
-            paddleMove = constants.NONE;
-            gameStatus = constants.GAME.STATUS.START;
-            window.requestAnimationFrame(animate);
-
-            doc.addEventListener('keydown', function(event) {
-                if (event.keyCode === constants.KEYBOARD.LEFT) {
-                    paddleMove = constants.DIRECTION.LEFT;
-                }
-
-                if (event.keyCode === constants.KEYBOARD.RIGHT) {
-                    paddleMove = constants.DIRECTION.RIGHT;
-                }
-            });
-
-            doc.addEventListener('keyup', function(event) {
-                console.log(event.keyCode);
-                if (event.keyCode == constants.KEYBOARD.LEFT) {
-                    paddleMove = constants.NONE;
-                }
-
-                if (event.keyCode == constants.KEYBOARD.RIGHT) {
-                    paddleMove = constants.NONE;
-                }
-            });
-        };
-
-        endGame = function() {
-            gameStatus = constants.GAME.STATUS.STOP;
-            drawer.ctx.fillText('The End!', drawer.canvasWidth / 2, drawer.canvasHeight / 2);
-        };
-
-        animate = function() {
-            gameStatus = constants.GAME.STATUS.PLAYING;
-            drawer.clearArea();
-            board.draw(
-                conf.BOARD.BLOCK.FILL_COLOR,
-                conf.BOARD.BLOCK.STROKE_COLOR
-            );
-            board.displayScore(score);
-            ball.move([paddle]);
-            ball.draw();
-            paddle.move(paddleMove);
-            board.collision([ball], function() {
-                score += 2;
-            });
-            paddle.draw(conf.PADDLE.FILL_COLOR, conf.PADDLE.STROKE_COLOR);
-
-            if (board.isEmpty() || ball.isDestroyed()) {
-                gameStatus = constants.GAME.STATUS.END;
-            }
-
-            if (constants.GAME.STATUS.PLAYING === gameStatus) {
+            startGame = function() {
+                paddleMove = constants.NONE;
+                gameStatus = constants.GAME.STATUS.START;
                 window.requestAnimationFrame(animate);
-            }
-        };
 
-        startGame();
+                document.addEventListener('keydown', function(event) {
+                    if (event.keyCode === constants.KEYBOARD.LEFT) {
+                        paddleMove = constants.DIRECTION.LEFT;
+                    }
+
+                    if (event.keyCode === constants.KEYBOARD.RIGHT) {
+                        paddleMove = constants.DIRECTION.RIGHT;
+                    }
+                });
+
+                document.addEventListener('keyup', function(event) {
+                    console.log(event.keyCode);
+                    if (event.keyCode == constants.KEYBOARD.LEFT) {
+                        paddleMove = constants.NONE;
+                    }
+
+                    if (event.keyCode == constants.KEYBOARD.RIGHT) {
+                        paddleMove = constants.NONE;
+                    }
+                });
+            };
+
+            endGame = function() {
+                gameStatus = constants.GAME.STATUS.STOP;
+                drawer.ctx.fillText('The End!', drawer.canvasWidth / 2, drawer.canvasHeight / 2);
+            };
+
+            animate = function() {
+                gameStatus = constants.GAME.STATUS.PLAYING;
+                drawer.clearArea();
+                board.draw(
+                    conf.BOARD.BLOCK.FILL_COLOR,
+                    conf.BOARD.BLOCK.STROKE_COLOR
+                );
+                board.displayScore(score);
+                ball.move([paddle]);
+                ball.draw();
+                paddle.move(paddleMove);
+                board.collision([ball], function() {
+                    score += 2;
+                });
+                paddle.draw(conf.PADDLE.FILL_COLOR, conf.PADDLE.STROKE_COLOR);
+
+                if (board.isEmpty() || ball.isDestroyed()) {
+                    gameStatus = constants.GAME.STATUS.END;
+                }
+
+                if (constants.GAME.STATUS.PLAYING === gameStatus) {
+                    window.requestAnimationFrame(animate);
+                }
+            };
+
+            startGame();
+        });
+
     });
